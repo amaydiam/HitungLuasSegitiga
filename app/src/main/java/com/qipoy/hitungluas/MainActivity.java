b275800ff62a4fbbfd9c5c5cc1ef5c4634d40054
qipoy <dota2.qipoy@gmail.com>
2016-01-23 01:33:14 -0500

    fixing numberexeption

---
+++ app/src/main/java/com/qipoy/hitungluas/MainActivity.java
@@ -0,0 +1,70 @@
+package com.qipoy.hitungluas;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edtPanjang, edtLebar;
+    private Button btnHitung;
+    private TextView txtLuas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("Luas Segitiga");
+
+        edtPanjang = (EditText) findViewById(R.id.edt_panjang);
+        edtLebar = (EditText) findViewById(R.id.edt_lebar);
+        btnHitung = (Button) findViewById(R.id.btn_hitung);
+        txtLuas = (TextView) findViewById(R.id.txt_luas);
+
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                String panjang = edtPanjang.getText().toString().trim();
+                String lebar = edtLebar.getText().toString().trim();
+
+                Double p = Double.parseDouble(panjang);
+                Double l = Double.parseDouble(lebar);
+
+                Double luas = p * l;
+                txtLuas.setText("Luas = "+luas +" Cm");
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
